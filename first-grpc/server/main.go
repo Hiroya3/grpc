74c4a4e986063bb8c12ce6f9380986e8051cc9d4
Hiroya3 <s.3.h.st.sp211@gmail.com>
2021-08-14 23:01:29 +0900

    main関数の実装

--- first-grpc/server/main.go
+++ first-grpc/server/main.go
@@ -4,7 +4,14 @@ import (
 	"context"
 	"errors"
 	pb "github.com/Hiroya3/grpc/first-grpc/grpc-define"
+	"google.golang.org/grpc"
+	"log"
+	"net"
 	"time"
+)
+
+const (
+	port = ":50051"
 )
 
 type server struct {
@@ -21,5 +28,14 @@ func (b *server) GetBornYear(_ context.Context, age *pb.Age) (*pb.Year, error) {
 }
 
 func main() {
-
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterBornYearServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
